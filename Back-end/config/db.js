e53404f9c3ae9119105a209bbfa684a2d627e1b4
harshitvatsa <harshitvatsa284@gmail.com>
2025-04-22 07:54:42 +0530

    some updates

--- Back-end/config/db.js
+++ Back-end/config/db.js
@@ -5,7 +5,13 @@ const MONGODB_URL = process.env.MONGODB_URL; // Use MONGODB_URL
 
 const connectionToMongodb = () => {
     console.log("MongoDB URL:", MONGODB_URL); // Log the URL to check if it's defined
-    mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    if (!MONGODB_URL) {
+        console.error("MongoDB URL is not defined. Please set the MONGODB_URL environment variable.");
+        process.exit(1); // Exit the process if the URI is not set
+    }
+
+    // Remove deprecated options
+    mongoose.connect(MONGODB_URL)
         .then(() => console.log("MongoDB connection successful"))
         .catch(err => console.log("MongoDB connection unsuccessful", err));
 };
